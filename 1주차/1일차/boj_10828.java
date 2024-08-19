ae2ed950a62d6d0447c95154f0f260731ceacd98
djsumin <136299367+djsumin@users.noreply.github.com>
2024-08-19 19:39:23 +0900

    Update boj_10828.java

--- 1주차/1일차/boj_10828.java
+++ 1주차/1일차/boj_10828.java
@@ -1,1 +1,43 @@
- 
+ package org.example;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        Stack<Integer> stack = new Stack<>();
+
+        for(int i=0;i<n;i++){
+            String order = sc.next();
+            if(order.equals("push")){
+                int x = sc.nextInt();
+                stack.push(x);
+            }else if(order.equals("pop")){
+                if(stack.isEmpty()){
+                    System.out.println(-1);
+                }else{
+                    System.out.println(stack.pop());
+                }
+            }else if(order.equals("size")){
+                System.out.println(stack.size());
+            }else if(order.equals("empty")){
+                if(stack.isEmpty()){
+                    System.out.println(1);
+                }else{
+                    System.out.println(0);
+                }
+            }else if(order.equals("top")){
+                if(stack.isEmpty()){
+                    System.out.println(-1);
+                }else{
+                    System.out.println(stack.peek());
+                }
+            }
+        }
+
+
+    }
+}
